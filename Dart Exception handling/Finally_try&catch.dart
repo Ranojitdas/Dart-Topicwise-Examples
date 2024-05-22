143d596f774bf9fd990d6710cc3546bf7d7eae8c
Ranojitdas <145350613+Ranojitdas@users.noreply.github.com>
2024-05-23 01:07:33 +0530

    Update Finally_try&catch.dart

--- Dart Exception handling/Finally_try&catch.dart
+++ Dart Exception handling/Finally_try&catch.dart
@@ -3,7 +3,7 @@ void main() {
   int b = 0;
   int res;
   try {
-    res = a ~/ b;
+  res = a ~/ b;                 //   (12/0) gives infinite value which cause error
   } on UnsupportedError {
     print('Cannot divide by zero');
   } catch (ex) {
