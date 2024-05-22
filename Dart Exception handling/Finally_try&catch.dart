caf7f6037c9eecf2de209652fdec76bcb6ca4ea7
Ranojitdas <145350613+Ranojitdas@users.noreply.github.com>
2024-05-23 01:04:59 +0530

    Add files via upload

---
+++ Dart Exception handling/Finally_try&catch.dart
@@ -0,0 +1,14 @@
+void main() {
+  int a = 12;
+  int b = 0;
+  int res;
+  try {
+    res = a ~/ b;
+  } on UnsupportedError {
+    print('Cannot divide by zero');
+  } catch (ex) {
+    print(ex);
+  } finally {
+    print('Finally block always executed');
+  }
+}
